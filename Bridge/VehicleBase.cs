587d2689cb837428e764823309dc931d43a7c3cb
Mert Gültekin <149678832+Steez95@users.noreply.github.com>
2025-09-30 11:04:54 +0200

    VehicleBase
    VehicleBase si updated witch a validation

--- Bridge/VehicleBase.cs
+++ Bridge/VehicleBase.cs
@@ -8,10 +8,24 @@ namespace Bridge
 {
     public abstract class VehicleBase
     {
+        private string _licenseplate;
+
         /// <summary>
         /// Gets or sets the license plate number of the vehicle.
+        /// License plate must not exceed 7 characters, otherwise and excepition is thrown.
         /// </summary>
-        public string Licenseplate { get; set; }
+        public string Licenseplate 
+        { 
+            get => _licenseplate;
+            set 
+            {
+                if (value != null && value.Length > 7)
+                {
+                    throw new ArgumentException("License plate cannot exceed 7 characters.");
+                }
+                _licenseplate = value;
+            }
+        }
 
         /// <summary>
         /// Gets or sets the date associated with this instance.
